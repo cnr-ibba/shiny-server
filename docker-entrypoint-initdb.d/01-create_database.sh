61783673711484ad7bce809e93db5c5bfcfead88
Paolo Cozzi <bunop@libero.it>
2020-02-18 17:24:32 +0100

    :card_file_box: configure database

---
+++ docker-entrypoint-initdb.d/01-create_database.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+mysql --user=root --password=${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE ${SHINY_DATABASE} DEFAULT CHARACTER SET 'utf8' DEFAULT COLLATE 'utf8_bin'"
+mysql --user=root --password=${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON ${SHINY_DATABASE}.* to '${SHINY_USER}'@'%' IDENTIFIED BY '${SHINY_PASSWORD}'"
+mysql --user=root --password=${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON ${SHINY_DATABASE}.* to '${SHINY_USER}'@'localhost' IDENTIFIED BY '${SHINY_PASSWORD}'"
