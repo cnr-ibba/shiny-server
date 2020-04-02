541d69acfbac633c01b2efdbf28bbe70353ce44b
Paolo Cozzi <bunop@libero.it>
2020-04-02 17:38:46 +0200

    :white_check_mark: test shiny app list

--- django-data/shiny/serve/tests/test_views.py
+++ django-data/shiny/serve/tests/test_views.py
@@ -9,7 +9,7 @@ Created on Thu Apr  2 16:40:39 2020
 from django.test import TestCase, Client
 from django.urls import resolve, reverse
 
-from ..views import IndexView, ShinyAppView
+from ..views import IndexView, ShinyAppView, ShinyAppListView
 
 
 class BaseMixin():
@@ -66,3 +66,56 @@ class ShinyAppViewTestCase(BaseMixin, TestCase):
         response = client.get(url)
 
         self.assertEqual(response.status_code, 200)
+
+
+class ShinyAppListViewTestCase(BaseMixin, TestCase):
+    def setUp(self):
+        # call base method
+        super().setUp()
+
+        # set the base url
+        self.url = reverse('applications')
+
+    def test_url_resolves_view(self):
+        view = resolve('/applications/')
+        self.assertIsInstance(view.func.view_class(), ShinyAppListView)
+
+    def test_get_public_app(self):
+        # I can get a public app whitout logging
+        client = Client()
+        response = client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+
+        # ok get queryset from response.context and test for the public app
+        qs = response.context['shinyapp_list']
+        self.assertEqual(qs.count(), 1)
+
+        public = qs.first()
+        self.assertEqual(public.slug, 'reactivity')
+
+    def test_get_admin_app(self):
+        # login as admin
+        client = Client()
+        client.login(username='test', password='test')
+        response = client.get(self.url)
+
+        # ok get all app for the admin
+        qs = response.context['shinyapp_list']
+        self.assertEqual(qs.count(), 3)
+
+    def test_my_app(self):
+        # login as a normal user
+        client = Client()
+        client.login(username='test2', password='test2')
+        response = client.get(self.url)
+
+        # get only my app and the public app
+        qs = response.context['shinyapp_list']
+        self.assertEqual(qs.count(), 2)
+
+        reference = ['reactivity', 'shiny-text']
+        test = sorted([app.slug for app in qs])
+
+        # test for expected slugs
+        self.assertEqual(reference, test)
